a68d10196b950f8dd188e0282e82f97f0432dded
kea6t <keasay26@gmail.com>
2022-07-12 15:58:17 -0400

    added first prompt question

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name fool?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const pageHTML = generatePage(name, github);
+// const pageHTML = generatePage(name, github);
 
 
 
-fs.writeFile('./index.html', pageHTML, err => {
-    if(err) throw new Error(err);
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if(err) throw new Error(err);
 
-    console.log('Portfolio Complete! Check out index.html to see the output');
-});
+//     console.log('Portfolio Complete! Check out index.html to see the output');
+// });
+
